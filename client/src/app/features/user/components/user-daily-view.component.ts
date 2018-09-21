4c157d1cf1a7808a0de3dc4e2cb201be0adb2921
davide <davide.schuen@abuscom.com>
2018-09-21 23:41:24 +0200

    changed amexio import

--- client/src/app/features/user/components/user-daily-view.component.ts
+++ client/src/app/features/user/components/user-daily-view.component.ts
@@ -3,17 +3,27 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'ga-user-daily-view',
   template: `
-    <p>
-      user-daily-view works!
-    </p>
+    <amexio-d3-chart-donut
+      [title]="'My Daily Activity'"
+      [data] = "defultColorData"
+    </amexio-d3-chart-donut>
   `,
   styles: []
 })
 export class UserDailyViewComponent implements OnInit {
 
+  areaChartData: any[];
+
   constructor() { }
 
   ngOnInit() {
+    this.areaChartData = [
+      ['Year', 'Sales', 'Expenses'],
+      ['2013',  1000,      400],
+      ['2014',  1170,      460],
+      ['2015',  660,       1120],
+      ['2016',  1030,      540]
+    ];
   }
 
 }
