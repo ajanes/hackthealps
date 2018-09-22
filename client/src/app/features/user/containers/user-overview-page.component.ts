99e8710cf9b0f22caff43c431412f3298a5b3f3b
davide <davide.schuen@abuscom.com>
2018-09-22 21:33:28 +0200

    implemented daily dashboard view

--- client/src/app/features/user/containers/user-overview-page.component.ts
+++ client/src/app/features/user/containers/user-overview-page.component.ts
@@ -8,18 +8,18 @@ import {WeeklyView} from '../model/weekly-view';
   selector: 'ga-user-overview-page',
   template: `
     <p-tabView>
-      <p-tabPanel header="Daily">
-        Not yet implemented
+      <p-tabPanel header="Daily"  [selected]="true">
+        <ga-user-daily-view [model]="dailyView$ | async"></ga-user-daily-view>
       </p-tabPanel>
-      <p-tabPanel header="Weekly" [selected]="true">
+      <p-tabPanel header="Weekly">
         <ga-user-weekly-view [model]="weeklyView$ | async"></ga-user-weekly-view>
       </p-tabPanel>
       <p-tabPanel header="Monthly">
         Not yet implemented
       </p-tabPanel>
     </p-tabView>
-    
-        
+
+
   `,
   styles: []
 })
@@ -27,12 +27,15 @@ export class UserOverviewPageComponent implements OnInit {
 
   viewType: ViewType;
   weeklyView$: Observable<WeeklyView>;
+  dailyView$: Observable<any>;
 
-  constructor(private userService: UserService) { }
+  constructor(private userService: UserService) {
+  }
 
   ngOnInit() {
-    this.viewType = ViewType.WEEKLY;
+    this.viewType = ViewType.DAILY;
     this.weeklyView$ = this.userService.getWeeklyView$();
+    this.dailyView$ = this.userService.getDailyView$();
   }
 
 }
